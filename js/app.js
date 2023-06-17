7597ea7c3ffc6c299c30104ae5e61cd4b5964ef4
Roylan Pais <roylanpais@gmail.com>
2023-06-17 15:13:26 +0530

    made files from udacity Starter Code and edited css

---
+++ js/app.js
@@ -0,0 +1,54 @@
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+// selects all sections
+const sections = document.querySelectorAll('section');
+const menu = document.getElementById("navbar__list");
+const topButton = document.getElementById('top-button');
+// hide variable used to hide the navigation bar
+var hide;
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
